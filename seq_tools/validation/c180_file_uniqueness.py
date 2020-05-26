9a8bcd93a026e6399e7ceece9525f4fd1afa0d10
Junjun Zhang <junjun.ca@gmail.com>
2020-05-26 14:42:55 -0400

    update another test case to cover VALID for c180_file_uniqueness

--- seq_tools/validation/c180_file_uniqueness.py
+++ seq_tools/validation/c180_file_uniqueness.py
@@ -15,7 +15,6 @@
     along with this program. If not, see <https://www.gnu.org/licenses/>.
 
     Authors:
-        Junjun Zhang <junjun.zhang@oicr.on.ca>
         Linda Xiang <linda.xiang@oicr.on.ca>
 """
 
