9f6ee72cfdb604ef112ed87352cd8c28bd11c877
Junjun Zhang <junjun.ca@gmail.com>
2020-12-09 14:43:47 -0500

    added a new check: file name pattern

---
+++ seq_tools/validation/c260_filename_pattern.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Copyright (c) 2020, Ontario Institute for Cancer Research (OICR).
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+    Authors:
+        Linda Xiang <linda.xiang@oicr.on.ca>
+        Junjun Zhang <junjun.zhang@oicr.on.ca>
+"""
+
+
+import os
+import re
+from base_checker import BaseChecker
+
+
+class Checker(BaseChecker):
+    def __init__(self, ctx, metadata):
+        super().__init__(
+            ctx=ctx,
+            metadata=metadata,
+            checker_name=__name__,
+            depends_on=[  # dependent checks
+                'c190_no_extra_files',
+                'c210_no_path_in_filename'
+            ]
+        )
+
+    @BaseChecker._catch_exception
+    def check(self):
+        # status already set at initiation
+        if self.status:
+            return
+
+        if not self.metadata.get('files'):
+            message = "Missing 'files' section in the metadata JSON"
+            self.logger.info(f'[{self.checker}] {message}')
+            self.message = message
+            self.status = 'INVALID'
+            return
+
+        filename_with_mismatch_pattern = set()
+        for fl in self.metadata.get('files'):
+            if 'fileName' not in fl or not fl['fileName']:
+                message = "Required field 'fileName' is not found or not populated in 'files' " \
+                    "section of the metadata JSON."
+                self.logger.info(f'[{self.checker}] {message}')
+                self.message = message
+                self.status = 'INVALID'
+                return
+
+            if not re.match(r'^[A-Za-z0-9]{1}[A-Za-z0-9_\.\-]+$', fl['fileName']):
+                filename_with_mismatch_pattern.add(fl['fileName'])
+
+        if filename_with_mismatch_pattern:
+            message = "'fileName' must match expected pattern '^[A-Za-z0-9]{1}[A-Za-z0-9_\\.\\-]+$' in the 'files' " \
+                "section of the metadata, offending name(s): '%s'" % ', '.join(sorted(filename_with_mismatch_pattern))
+            self.logger.info(f'[{self.checker}] {message}')
+            self.message = message
+            self.status = 'INVALID'
+        else:
+            self.status = 'PASS'
+            message = "'fileName' matches expected pattern '^[A-Za-z0-9]{1}[A-Za-z0-9_\\.\\-]+$' in 'files' " \
+                "section. Validation status: PASS"
+            self.message = message
+            self.logger.info(f'[{self.checker}] {message}')
