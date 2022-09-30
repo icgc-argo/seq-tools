c0e395ede3d78f5fc3e956ca498c8cbab99a7fe0
edsu7 <22638361+edsu7@users.noreply.github.com>
2022-09-30 15:19:07 -0400

    adding in additional test scenarios

--- tests/test_validations.py
+++ tests/test_validations.py
@@ -41,9 +41,9 @@ def test_validate(submission):
     if not seq_files:
         cli_option += ['-d', os.path.join(test_dir, 'seq-data')]
 
-    if str(Path(metadata_file).parent).endswith('skip-md5'):
+    if 'skip-md5' in str(Path(metadata_file).parent):
         cli_option += ['--skip_md5sum_check']
-    if str(Path(metadata_file).parent).endswith('skip-strand'):
+    if 'skip-strand' in str(Path(metadata_file).parent):
         cli_option += ['--skip_strandedness_check']
 
     runner.invoke(main, cli_option)
